d17ec3083d0cce5614e2bf52aabeaeaf6621afbd
cb <devnull@localhost>
2007-06-16 15:04:51 +0200

    [svn] add (C) headers

--- src/window_play.c
+++ src/window_play.c
@@ -1,3 +1,18 @@
+/*
+ *  tenace - bridge hand viewer and editor
+ *  Copyright (C) 2005-2007 Christoph Berg <cb@df7cb.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
 #include <assert.h>
 
 #include "bridge.h"
