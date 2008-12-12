7ed1ae5cadc54de5d7682e9377b523660f605a69
Christoph Berg <cb@df7cb.de>
2008-12-12 23:21:19 +0100

    make border width et al configurable

--- src/print.c
+++ src/print.c
@@ -20,6 +20,10 @@
 #include "functions.h"
 #include "window_board.h"
 
+static GtkPrintSettings *settings = NULL;
+
+/* standard printing */
+
 #define STRIDE 0.25 * 72
 #define L 1.0 * 72
 #define C 2.0 * 72
@@ -99,7 +103,44 @@ draw_page (GtkPrintOperation *operation,
 }
 
 static void
-arrow (cairo_t *cr, int n, seat dir, double size)
+begin_print (GtkPrintOperation *operation,
+	     GtkPrintContext   *context,
+	     gpointer           user_data)
+{
+	gtk_print_operation_set_n_pages (operation, 1);
+}
+
+void
+on_menu_print_activate (void)
+{
+	GtkPrintOperation *print;
+	GtkPrintOperationResult res;
+
+	print = gtk_print_operation_new ();
+
+	if (settings != NULL)
+		gtk_print_operation_set_print_settings (print, settings);
+
+	g_signal_connect (print, "begin_print", G_CALLBACK (begin_print), NULL);
+	g_signal_connect (print, "draw_page", G_CALLBACK (draw_page), NULL);
+
+	res = gtk_print_operation_run (print, GTK_PRINT_OPERATION_ACTION_PRINT_DIALOG,
+			GTK_WINDOW (win->window), NULL);
+
+	if (res == GTK_PRINT_OPERATION_RESULT_APPLY)
+	{
+		if (settings != NULL)
+			g_object_unref (settings);
+		settings = g_object_ref (gtk_print_operation_get_print_settings (print));
+	}
+
+	g_object_unref (print);
+}
+
+/* magic cards */
+
+static void
+arrow (cairo_t *cr, int n, int background, seat dir, double size)
 {
 #define ARHEAD 0.0
 #define ARWIDTH 0.2
@@ -107,7 +148,7 @@ arrow (cairo_t *cr, int n, seat dir, double size)
 	cairo_translate (cr, 0.5*size, 0.5*size);
 
 	/* background box */
-	if ((n + (n - 1) / 6) % 2 == 0) {
+	if (background) {
 		cairo_set_source_rgb (cr, 0.8, 0.8, 0.8);
 		cairo_move_to (cr, -0.5*size, -0.5*size);
 		cairo_line_to (cr, 0.5*size, -0.5*size);
@@ -122,9 +163,9 @@ arrow (cairo_t *cr, int n, seat dir, double size)
 	cairo_rotate (cr, ((int)dir - 2) * M_PI_2);
 
 	if (n % 2)
-		cairo_set_source_rgb (cr, 0.7, 0.4, 0.4);
+		cairo_set_source_rgb (cr, 0.7, 0.4, 0.4); /* red */
 	else
-		cairo_set_source_rgb (cr, 0.4, 0.4, 0.7);
+		cairo_set_source_rgb (cr, 0.4, 0.4, 0.7); /* blue */
 
 	cairo_move_to (cr, 0.0, -0.5*size);
 	cairo_line_to (cr, 0.5*size, -ARHEAD*size);
@@ -165,20 +206,55 @@ cairo_clip_rectangle (cairo_t *cr, double x0, double y0, double x1, double y1)
 	cairo_clip (cr);
 }
 
-#define ARROWSIZE 18.0
+/*  row_height
+ ***************** border
+ *xxx   xxx   xxx* arrow_size
+ *xxx   xxx   xxx*
+ *   xxx   xxx   *
+ *   xxx   xxx   * col_width
+ * arrow_columns *
+ *    TEXTSEP    *
+ *               *
+ *               *
+ *               *
+ *               *
+ *****************/
+
+#define TEXTSEP 20.0
+
+struct magic_config_t {
+	/* custom print settings */
+	GtkSpinButton* magic_columns, * magic_rows;
+	GtkToggleButton* magic_no_header, * magic_page_header, * magic_suit_header;
+	GtkToggleButton* magic_mark_cards;
+	GtkSpinButton* magic_from, * magic_to;
+	GtkSpinButton* magic_border;
+	int columns, rows;
+	int header;
+	int mark_cards;
+	int from, to;
+	double border;
+	/* metrics */
+	double col_width, row_height;
+	int arrow_columns;
+	double arrow_size;
+};
+static struct magic_config_t mc;
 
 static void
 magic_card_half (cairo_t *cr, window_board_t *win, card c)
 {
+	int n_boards = mc.to - mc.from + 1;
 	int n;
-	for (n = 0; n < win->n_boards; n++) {
-		if (n >= 24)
-			break;
-
+	for (n = 0; n < n_boards; n++) {
 		cairo_save (cr);
-		cairo_translate (cr, (n % 6) * ARROWSIZE, (n / 6) * ARROWSIZE);
+		int col = n % mc.arrow_columns;
+		int row = n / mc.arrow_columns;
+		cairo_translate (cr, col * mc.arrow_size + mc.border,
+				row * mc.arrow_size + mc.border);
 
-		arrow (cr, n + 1, win->boards[n]->dealt_cards[c], ARROWSIZE);
+		arrow (cr, n + mc.from, (col + row) % 2,
+				win->boards[n + mc.from - 1]->dealt_cards[c], mc.arrow_size);
 		cairo_restore (cr);
 
 		/* draw title */
@@ -187,16 +263,17 @@ magic_card_half (cairo_t *cr, window_board_t *win, card c)
 
 		cairo_select_font_face (cr, "Sans",
 			CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-		cairo_set_font_size (cr, 8);
-		cairo_clip_rectangle (cr, 0.0, 0.0, 6 * ARROWSIZE, 9 * ARROWSIZE);
-		cairo_move_to (cr, 2.0, 4.45 * ARROWSIZE);
+		cairo_set_font_size (cr, 8.0);
+		cairo_clip_rectangle (cr, mc.border, mc.border,
+				mc.row_height - mc.border, mc.col_width - mc.border);
+		cairo_move_to (cr, mc.border, 0.5 * mc.col_width - 2.0);
 		cairo_show_text (cr, win->title);
-
 		cairo_text_extents (cr, win->title, &ext);
+
 		/* clip subtitle to card width - title width */
-		cairo_clip_rectangle (cr, 0.0, 0.0,
-			6 * ARROWSIZE - ext.x_advance - 4.0, 9 * ARROWSIZE);
-		cairo_move_to (cr, 2.0, 4.85 * ARROWSIZE);
+		cairo_clip_rectangle (cr, mc.border, mc.border,
+			mc.row_height - ext.x_advance - mc.border - 4.0, mc.col_width - mc.border);
+		cairo_move_to (cr, mc.border, 0.5 * mc.col_width + 8.0);
 		cairo_show_text (cr, win->subtitle);
 		cairo_stroke (cr);
 
@@ -211,23 +288,11 @@ magic_card (cairo_t *cr, window_board_t *win, card c)
 	magic_card_half (cr, win, c);
 	/* lower part */
 	cairo_save (cr);
-	cairo_translate (cr, 6 * ARROWSIZE, 9 * ARROWSIZE);
+	cairo_translate (cr, mc.row_height, mc.col_width);
 	cairo_rotate (cr, M_PI);
 	magic_card_half (cr, win, c);
 	cairo_restore (cr);
 }
-
-struct magic_config_t {
-	GtkSpinButton* magic_columns;
-	GtkSpinButton* magic_rows;
-	GtkToggleButton* magic_no_header;
-	GtkToggleButton* magic_page_header;
-	GtkToggleButton* magic_suit_header;
-	int columns;
-	int rows;
-	int header;
-};
-static struct magic_config_t magic_config;
 
 static void
 magic_draw_page (GtkPrintOperation *operation,
@@ -238,22 +303,20 @@ magic_draw_page (GtkPrintOperation *operation,
 {
 	cairo_t *cr;
 	cr = gtk_print_context_get_cairo_context (context);
-	double col_width = gtk_print_context_get_width (context) / magic_config.columns;
-	double row_height = gtk_print_context_get_height (context) / magic_config.rows;
 
 	static int cc, print_head;
 	if (page_nr == 0) { /* restart printing */
 		cc = 51;
-		print_head = (magic_config.header > 0);
+		print_head = (mc.header > 0);
 	}
-	if (magic_config.header == 1) /* new page */
+	if (mc.header == 1) /* new page */
 		print_head = 1;
 
 	int r, c;
-	for (c = 0; c < magic_config.columns; c++) {
-		for (r = 0; r < magic_config.rows && cc >= 0; r++) {
+	for (c = 0; c < mc.columns; c++) {
+		for (r = 0; r < mc.rows && cc >= 0; r++) {
 			if (print_head) { /* heading */
-				cairo_move_to (cr, c * col_width + 80.0, 80.0);
+				cairo_move_to (cr, c * mc.col_width + 80.0, 80.0);
 				cairo_select_font_face (cr, "Symbol",
 					CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
 				cairo_set_font_size (cr, 40.0);
@@ -263,57 +326,28 @@ magic_draw_page (GtkPrintOperation *operation,
 			}
 
 			cairo_save (cr);
-			cairo_translate (cr,
-				col_width * c + (col_width - 9 * ARROWSIZE) / 2,
-				row_height * (r + 1) - (row_height - 6 * ARROWSIZE) / 2);
+			cairo_translate (cr, mc.col_width * c, mc.row_height * (r + 1));
 			cairo_rotate (cr, -M_PI_2);
 			magic_card (cr, win, cc);
 
-			/* debugging: print card */
-			cairo_move_to (cr, 6, -2);
-			cairo_set_font_size (cr, 10);
-			cairo_select_font_face (cr, "Symbol",
-				CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-			cairo_show_text (cr, trump_str[SUIT(cc)]);
-			cairo_select_font_face (cr, "Sans",
-				CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-			cairo_show_text (cr, _(rank_string (RANK(cc))));
+			if (mc.mark_cards) {
+				cairo_move_to (cr, 6, -2);
+				cairo_set_font_size (cr, 10);
+				cairo_select_font_face (cr, "Symbol",
+					CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+				cairo_show_text (cr, trump_str[SUIT(cc)]);
+				cairo_select_font_face (cr, "Sans",
+					CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+				cairo_show_text (cr, _(rank_string (RANK(cc))));
+			}
 
 			cairo_restore (cr);
 			
 			cc--; /* next card */
-			if (RANK(cc) == cardA && magic_config.header == 2)
+			if (RANK(cc) == cardA && mc.header == 2)
 				print_head = 1;
 		}
 	}
-}
-
-static void
-begin_print (GtkPrintOperation *operation,
-	     GtkPrintContext   *context,
-	     gpointer           user_data)
-{
-	gtk_print_operation_set_n_pages (operation, 1);
-}
-
-static void
-magic_begin_print (GtkPrintOperation *operation,
-	     GtkPrintContext   *context,
-	     gpointer           user_data)
-{
-	if (magic_config.header == 0) {
-		gtk_print_operation_set_n_pages
-			(operation, ceil (52.0 / (magic_config.columns * magic_config.rows)));
-	} else if (magic_config.header == 1) {
-		gtk_print_operation_set_n_pages
-			(operation, ceil (52.0 / (magic_config.columns * magic_config.rows - 1)));
-	} else {
-		gtk_print_operation_set_n_pages
-			(operation, ceil (56.0 / (magic_config.columns * magic_config.rows)));
-	}
-	gtk_print_operation_set_use_full_page (operation, TRUE);
-	gtk_print_operation_set_show_progress (operation, TRUE);
-	//gtk_print_operation_set_export_filename (operation, "foo");
 }
 
 static GObject *
@@ -323,11 +357,18 @@ magic_custom_create (GtkPrintOperation *operation,
 	GladeXML *xml = glade_xml_new(PACKAGE_DATA_DIR "/" PACKAGE "/" PACKAGE ".glade", "magic_options_table", NULL);
 	GtkWidget *table = glade_xml_get_widget (xml, "magic_options_table");
 	assert (table);
-	magic_config.magic_rows = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_rows"));
-	magic_config.magic_columns = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_columns"));
-	magic_config.magic_no_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_no_header"));
-	magic_config.magic_page_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_page_header"));
-	magic_config.magic_suit_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_suit_header"));
+	mc.magic_rows = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_rows"));
+	mc.magic_columns = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_columns"));
+	mc.magic_no_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_no_header"));
+	mc.magic_page_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_page_header"));
+	mc.magic_suit_header = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_suit_header"));
+	mc.magic_mark_cards = GTK_TOGGLE_BUTTON (glade_xml_get_widget (xml, "magic_mark_cards"));
+	mc.magic_from = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_from"));
+	gtk_spin_button_set_range (mc.magic_from, 1, win->n_boards);
+	mc.magic_to = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_to"));
+	gtk_spin_button_set_range (mc.magic_to, 1, win->n_boards);
+	gtk_spin_button_set_value (mc.magic_to, win->n_boards);
+	mc.magic_border = GTK_SPIN_BUTTON (glade_xml_get_widget (xml, "magic_border"));
 	g_object_unref (xml);
 	return (GObject *) table;
 }
@@ -335,40 +376,50 @@ magic_custom_create (GtkPrintOperation *operation,
 static void
 magic_custom_apply (GtkPrintOperation *operation, GtkWidget *table, gpointer user_data)
 {
-	magic_config.columns = gtk_spin_button_get_value_as_int (magic_config.magic_columns);
-	magic_config.rows = gtk_spin_button_get_value_as_int (magic_config.magic_rows);
-	magic_config.header = gtk_toggle_button_get_active (magic_config.magic_no_header) ? 0 :
-			      gtk_toggle_button_get_active (magic_config.magic_page_header) ? 1 : 2;
+	mc.columns = gtk_spin_button_get_value_as_int (mc.magic_columns);
+	mc.rows = gtk_spin_button_get_value_as_int (mc.magic_rows);
+	mc.header = gtk_toggle_button_get_active (mc.magic_no_header) ? 0 :
+			      gtk_toggle_button_get_active (mc.magic_page_header) ? 1 : 2;
+	mc.mark_cards = gtk_toggle_button_get_active (mc.magic_mark_cards);
+	mc.from = gtk_spin_button_get_value_as_int (mc.magic_from);
+	mc.to = gtk_spin_button_get_value_as_int (mc.magic_to);
+	if (mc.to < mc.from)
+		mc.to = mc.from;
+	mc.border = gtk_spin_button_get_value_as_float (mc.magic_border);
 }
 
-static GtkPrintSettings *settings = NULL;
-
-void
-on_menu_print_activate (void)
+static void
+magic_begin_print (GtkPrintOperation *operation,
+	     GtkPrintContext   *context,
+	     gpointer           user_data)
 {
-	GtkPrintOperation *print;
-	GtkPrintOperationResult res;
-	printf ("printing...\n");
-
-	print = gtk_print_operation_new ();
-
-	if (settings != NULL) 
-		gtk_print_operation_set_print_settings (print, settings);
-
-	g_signal_connect (print, "begin_print", G_CALLBACK (begin_print), NULL);
-	g_signal_connect (print, "draw_page", G_CALLBACK (draw_page), NULL);
-
-	res = gtk_print_operation_run (print, GTK_PRINT_OPERATION_ACTION_PRINT_DIALOG,
-			GTK_WINDOW (win->window), NULL);
-
-	if (res == GTK_PRINT_OPERATION_RESULT_APPLY)
-	{
-		if (settings != NULL)
-			g_object_unref (settings);
-		settings = g_object_ref (gtk_print_operation_get_print_settings (print));
+	if (mc.header == 0) {
+		gtk_print_operation_set_n_pages
+			(operation, ceil (52.0 / (mc.columns * mc.rows)));
+	} else if (mc.header == 1) {
+		gtk_print_operation_set_n_pages
+			(operation, ceil (52.0 / (mc.columns * mc.rows - 1)));
+	} else {
+		gtk_print_operation_set_n_pages
+			(operation, ceil (56.0 / (mc.columns * mc.rows)));
 	}
+	gtk_print_operation_set_use_full_page (operation, TRUE);
+	gtk_print_operation_set_show_progress (operation, TRUE);
+	//gtk_print_operation_set_export_filename (operation, "foo");
+	mc.col_width = gtk_print_context_get_width (context) / mc.columns;
+	mc.row_height = gtk_print_context_get_height (context) / mc.rows;
 
-	g_object_unref (print);
+	/* find fitting number of arrow columns and corresponding arrow size */
+	int n_boards = mc.to - mc.from + 1;
+	int cols;
+	for (cols = 1; cols <= n_boards; cols++) {
+		mc.arrow_size = (mc.row_height - 2*mc.border) / cols;
+		int arrow_rows = ceil ((double) n_boards / cols);
+		if (mc.arrow_size * arrow_rows <= mc.col_width/2.0 - mc.border - TEXTSEP/2.0) {
+			mc.arrow_columns = cols;
+			break;
+		}
+	}
 }
 
 void
