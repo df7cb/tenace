ef11e8fd5099871dc2943646c983fbe0b91fddb9
Christoph Berg <myon@debian.org>
2013-10-07 13:22:34 +0200

    Update translations

--- src/unused.c
+++ src/unused.c
@@ -2,14 +2,22 @@
 static void
 translate_me ()
 {
+	/* West; translation should be a single letter */
 	_("W");
+	/* North; translation should be a single letter */
 	_("N");
+	/* East; translation should be a single letter */
 	_("E");
+	/* South; translation should be a single letter */
 	_("S");
 
 	_("NT");
+	/* Spades; translation should be a single letter */
 	_("Sp");
+	/* Hearts; translation should be a single letter */
 	_("He");
+	/* Diamonds; translation should be a single letter */
 	_("Di");
+	/* Clubs; translation should be a single letter */
 	_("Cl");
 }
