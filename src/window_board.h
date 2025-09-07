37732cf6b6e2c26acd820aa9374eab829a15b37d
Christoph Berg <myon@debian.org>
2025-09-07 17:02:33 +0000

    Fix board_window_init prototype

--- src/window_board.h
+++ src/window_board.h
@@ -83,7 +83,7 @@ void show_board (board *b, redraw_t redraw);
 void recently_used_add (char *filename);
 
 int board_window_append_board (window_board_t *win, board *b); /* returns number of appended board */
-void board_window_init ();
+void board_window_init (window_board_t *win);
 void board_window_set_style (window_board_t *win, int style, int card_width);
 void board_window_apply_style (window_board_t *win);
 
